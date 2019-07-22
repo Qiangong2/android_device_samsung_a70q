db13cc19767f53832ed3e65262c08ee1922e6689
ZhongQian TiaoGong <sassafrass@fasizzle.com>
2019-07-22 14:32:59 -0700

    Update BoardConfig.mk and add fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,18 +29,38 @@ TARGET_2ND_CPU_VARIANT := cortex-a75
 
 TARGET_USES_64_BIT_BINDER := true
 
+# bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sm6150
+TARGET_NO_BOOTLOADER := true
 
 # kernel
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/samsung/a70q
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_CONFIG := a70q_lineage_defconfig
 
+# partitions
+
+BOARD_FLASH_BLOCK_SIZE := 131072
+
 # platform
 TARGET_BOARD_PLATFORM := sm6150
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno612
 
+# recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_OTA_ASSERT_DEVICE := a70q
+TARGET_RECOVERY_FSTAB := device/samsung/a70q/recovery.fstab
+
+# twrp
 TARGET_RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
